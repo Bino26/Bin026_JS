03d3b2abf6a2e1027aeb1b57c163798ac416a233
Bino26 <hbino1999@gmail.com>
2023-06-21 05:05:47 +0000

    Build The Bookmarks DOM

--- BookMark App/script.js
+++ BookMark App/script.js
@@ -41,13 +41,56 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Build Bookmarks DOM
+const buildBookmarks = () => {
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    // Build Item
+    const item = document.createElement("div");
+    item.classList.add("item");
+    // Close Icon
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+    // Favicon / Link Container
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+
+    // Favicon
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://www.google.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("atl", "Favicon");
+
+    // link
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+  });
+};
+
 //Fetch BookMarks from LocalStorage
 
-const fetchBookMarks = () => {
+const fetchBookmarks = () => {
   // Get them if available
   if (localStorage.getItem("bookmarks")) {
     bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    // Create bookmarks array in localStorage
+    bookmarks = [
+      {
+        name: "Jacinto Design",
+        url: "https://jacinto.design",
+      },
+    ];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
+
+  buildBookmarks();
 };
 
 //Handle Data from Form
