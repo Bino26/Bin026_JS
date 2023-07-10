7a445fb6d94abb4460fdab8de74664a2a9d33fbd
Bino26 <hbino1999@gmail.com>
2023-07-10 01:41:35 +0000

    Test API and Key

---
+++ Movies App /script.js
@@ -0,0 +1,10 @@
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+
+async function getMovies(url) {
+  const response = await fetch(url);
+  const respData = await response.json();
+
+  console.log(respData);
+}
