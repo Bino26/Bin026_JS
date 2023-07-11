95c129055649a4ab4cffcb777dbdcc9a3a5e143c
Bino26 <hbino1999@gmail.com>
2023-07-11 00:01:45 +0000

    Working on the layout V1

--- Github Profiles App/script.js
+++ Github Profiles App/script.js
@@ -1,8 +1,34 @@
 const APIURL = `https://api.github.com/users/`;
+const main = document.getElementById("main");
 
-async function getUser(users) {
-  const resp = await fetch(APIURL + users);
+async function getUser(username) {
+  const resp = await fetch(APIURL + username);
   const respData = await resp.json();
-  console.log(`Name is:${respData.login}`);
+
+  createUserCard(respData);
 }
+function createUserCard(user) {
+  const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul class="info">
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos"></div>
+            </div>
+        </div>
+    `;
+
+  main.innerHTML = cardHTML;
+}
+
 getUser("Bino26");
