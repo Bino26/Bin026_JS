c9e28d06dee16fe4be0955205f4b43c51c9dd0a8
M'BAYI YANN BANGAH(BINO) <81714858+Bino26@users.noreply.github.com>
2023-08-07 14:54:04 +0000

    Add Joke App

---
+++ Joke App/script.js
@@ -0,0 +1,23 @@
+const header = document.getElementById("header") ;
+const text = document.getElementById("content") ;
+
+const getJoke = ()=>{
+    fetch("https://api.blablagues.net/?rub=blagues").then((res)=>res.json()).then((data)=>{
+    const content = data.data.content ;
+    console.log(content) ;
+
+    header.textContent = content.text_head ;
+    text.textContent = content.text !== "" ? content.text : content.text_hidden ;
+
+
+}
+); 
+
+}
+
+getJoke() ;
+
+document.body.addEventListener("click" ,getJoke) ;
+
+
+
